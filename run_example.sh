4c77e39b3f10ff78569ba5ce611d4698d63383f5
GGA23 <136722638+GGA23@users.noreply.github.com>
2026-01-28 13:32:36 +0800

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,2 +1,2 @@
-python3 main.py --cfg ./replication_configs/table3_bottom/reddit-body-gru-0.5.yaml
-python3 main.py --cfg ./replication_configs/table3_bottom/reddit-title-gru-0.1.yaml
+python3 main.py --cfg ./replication_configs/roland/reddit-body-gru-0.5.yaml
+python3 main.py --cfg ./replication_configs/roland/reddit-title-gru-0.1.yaml
